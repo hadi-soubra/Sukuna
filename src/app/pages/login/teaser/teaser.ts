710f6f292a887a123267358a40d7605f2b1fe804
hadi-soubra <hadisoubra@hotmail.com>
2026-07-12 11:05:38 +0300

    teaser page done

--- src/app/pages/login/teaser/teaser.ts
+++ src/app/pages/login/teaser/teaser.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-teaser',
@@ -6,4 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './teaser.html',
   styleUrl: './teaser.scss',
 })
-export class Teaser {}
+export class Teaser {
+  private readonly document = inject(DOCUMENT);
+  private readonly destroyRef = inject(DestroyRef);
+
+  private readonly fullHeadline = 'The Next Drop';
+  protected readonly headline = signal('');
+
+  protected readonly typingDone = signal(false);
+
+  constructor() {
+    this.document.body.classList.add('no-scroll');
+    this.destroyRef.onDestroy(() => {
+    this.document.body.classList.remove('no-scroll');
+    });
+
+    let charCount = 0;
+    const typingId = setInterval(() => {
+      charCount++;
+      this.headline.set(this.fullHeadline.slice(0, charCount));
+
+      if (charCount >= this.fullHeadline.length) {
+        clearInterval(typingId);
+        this.typingDone.set(true);
+
+      }
+    }, 80);
+
+    this.destroyRef.onDestroy(() => clearInterval(typingId));
+  }
+}
