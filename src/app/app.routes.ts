68b77d6640271628ea69decb9cb3d0e50fec9d78
hadi-soubra <hadisoubra@hotmail.com>
2026-07-30 22:53:11 +0300

    account page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {Shop} from './pages/shop/shop';
 import { Product } from './pages/product/product';
 import { Listing } from './pages/listing/listing';
 import { Cart } from './pages/cart/cart';
+import { Account } from './pages/account/account';
 import { AdminLogin } from './pages/admin/admin-login/admin-login';
 import { AdminPage } from './pages/admin/admin-page/admin-page';
 
@@ -29,6 +30,10 @@ export const routes: Routes = [
     {
         path: 'cart',
         component: Cart
+    },
+    {
+        path: 'account',
+        component: Account
     },
     {
         path: 'admin-login',
